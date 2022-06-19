2f45ca8e59dda8cb2aa63de124c0bbeda16e2f88
Cr4yfish <58395553+cr4yfish@users.noreply.github.com>
2022-06-20 00:46:07 +0200

    improved design

--- components/accessDenied.js
+++ components/accessDenied.js
@@ -1,11 +1,13 @@
 import LoginButton from "./LoginButton";
+import { Container, Spacer } from "@nextui-org/react"
 
 export default function AccessDenied() {
     return (
-        <>
+        <Container>
+            <Spacer />
             <h1>Access Denied</h1>
             <p>You do not have access to this content.</p>
             <LoginButton />
-        </>
+        </Container>
     )
 }
