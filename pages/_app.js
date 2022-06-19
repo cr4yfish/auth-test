12ee5aa183ad72032c70e53ec2d2cb7b43403c4a
Cr4yfish <58395553+cr4yfish@users.noreply.github.com>
2022-06-20 00:03:03 +0200

    init

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
 import '../styles/globals.css'
+import { SessionProvider } from "next-auth/react"
+import { NextUIProvider, createTheme } from '@nextui-org/react';
+import { ThemeProvider as NextThemesProvider } from 'next-themes';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+const darkTheme = createTheme({
+  type: "dark",
+});
+
+function MyApp({ Component, pageProps: { session, ...pageProps} }) {
+
+
+  return (
+    <SessionProvider session={pageProps.session} refetchInterval={0}>
+        <Component {...pageProps} />
+    </SessionProvider>
+
+
+  )
 }
-
 export default MyApp
